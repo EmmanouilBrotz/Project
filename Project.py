737eca05be168eb76cc372beda354f1451727cb3
EmmanouilBrotz <ebrotzakis@nyc.gr>
2023-11-24 11:10:08 +0200

    Finished up all functions, awaiting some user input failure checks

--- Project.py
+++ Project.py
@@ -1,4 +1,4 @@
-import csv #Will be used for importing/exporting .csv files.
+import csv #Will be used for importing/exporting .csv files. Also can be used for importing txt files through DictReader
 
 accounts = { #Account credentials for each user.
     "clerk" : "clerk123",
@@ -56,7 +56,7 @@ def display_options(username):
                      break
                   case _:
                      print("Incorrect value.")
-            else: #Last username should be Delivery anyway. It should be impossible to get here with a different username.
+            elif username == "delivery": 
                match choice:
                   case 1:
                      mark_order_as_delivered()
@@ -71,18 +71,51 @@ def new_order():
     description = input("Description of order of customer: ")
     date = input("Date of order (INPUT IN DD/MM/YY): ")
     price = input("Price of order: ")
-    order.update({"Name" : name, "Address" : address, "Description" : description, "Date" : date, "Price" : price, "ID" : len(orders) + 1, "Delivered" : False}) #Appending the order on the placeholder dictionary.
+    order.update({"Name" : name, "Address" : address, "Description" : description, "Date" : date, "Price" : price, "ID" : len(orders) + 1, "Delivered" : "False"}) #Appending the order on the placeholder dictionary.
     orders.append(order) #Appending the order on the list of orders.
 
-def import_from_file(): #WIP
-    print("Test2")
+def import_from_file(): #Import a csv or txt file into the orders list
+    file_type = input("What file type would you like to input? csv/txt: ")
+    file_type.strip()
+    if file_type.lower() == "csv":
+        file = input("Specify the name of the file: ")
+        try:
+            with open(file, 'r') as csvfile:
+                csvreader =   csv.DictReader(csvfile)
+                for row in csvreader:
+                    orders.append(row)
+        except FileNotFoundError:
+            print("Error: File doesn't exist.")      
+    elif file_type.lower() == "txt":
+        file = input("Specify the name of the file: ")
+        try:
+            with open(file, 'r') as txtfile:
+                txtreader = csv.DictReader(txtfile)
+                for row in txtreader:
+                    orders.append(row)
+        except FileNotFoundError:
+            print("Error: File doesn't exist.")
+    else:
+        print("Incorrect file type. Please try again.")
+    while True:
+        answer = input("Would you like to import another file? Y/N: ")
+        answer.strip()
+        if answer.lower() == "Y":
+            import_from_file()
+        elif answer.lower() == "N":
+            break
+        else:
+            print("Incorrect Value.")
+            continue
+        
+    
 def view_undelivered_orders(): #Checks the orders list for orders that have not been delivered, and prints them.
     for order in orders:
-       if order.get("Delivered") == False:
+       if str(order.get("Delivered")) == "False":
         print(order)
 def mark_order_as_delivered():
     for order in orders: #Printing all non-delivered orders so the deliver can check the ID of the order they delivered, to mark it as delivered.
-        if order.get("Delivered") == False:
+        if str(order.get("Delivered")) == "False":
             print(order)
     try:
         delivered = int(input("Please input ID of Delivered Order: ")) #Asking for input of delivered order ID.
@@ -90,13 +123,14 @@ def mark_order_as_delivered():
         print("You have most likely not input an integer. Please try again.")
     else:
         for order in orders:
-            if order.get("ID") == delivered:
-                order["Delivered"] = True
+            if int(order.get("ID")) == delivered:
+                order["Delivered"] = "True"
                 break
         else:
            print(f"Order with ID {delivered} either doesn't exist or has already been delivered.")
         while True:
             answer = input("Would you like to mark another delivery as Delivered? Y/N: ")
+            answer.strip()
             if answer.upper() == "Y":
                 mark_order_as_delivered()
             elif answer.upper() == "N":  
@@ -104,10 +138,89 @@ def mark_order_as_delivered():
             else:
                 print("Incorrect Value.")
                 continue
-def orders_overview():
-   pass
-def logistics_overview():
-   pass
+def orders_overview(): #Reads the Orders list, and acts accordingly to what the manager wants
+    try:
+        choice = int(input("Welcome to the Orders overview. Please select one of the choices available: \n 1. No. of orders from customer \n 2. No. of orders of specific day \n 3. Total amount of orders delivered \n 0. Exit \n Input your choice: "))
+    except ValueError as e:
+        print("You have most likely not input an integer. Please try again.")
+    else:
+        match choice:
+            case 1:
+                name = input("Please input the name of the customer: ")
+                count = 0
+                for order in orders:
+                    if order.get("Name") == name:
+                        print(order)
+                        count += 1
+                print(f"Total number of orders by {name}: {count}")
+            case 2:
+                date = input("Please input date to see orders that day (DD/MM/YY format): ")
+                count = 0
+                for order in orders:
+                    if order.get("Date") == date:
+                        print(order)
+                        count =+ 1
+                print(f"Total number of orders in {date}: {count}")
+            case 3:
+                count = 0
+                for order in orders:
+                    if str(order.get("Delivered")) == "True":
+                        print(order)
+                        count += 1
+                print(f"Total amount of orders delivered: {count}")
+            case 0:
+                display_options()
+            case _:
+                print("Choice not found.")
+    while True:
+        answer = input("Would you like to re-enter the Orders overview? Y/N: ")
+        answer.strip()
+        if answer == "Y":
+            orders_overview()
+        elif answer == "N":
+            break
+        else:
+            print("Incorrect choice. Try again.")
+            continue
+        
+   
+def logistics_overview(): #Reads from the orders list and gives financial data depending on what the manager wants.
+    try:
+        choice = int(input("Welcome to the Logistics overview. Please select one of the choices available: \n 1. Total price of orders from customer \n 2. Total price of orders in a day \n 0. Exit \n Input your choice: "))
+    except ValueError as e:
+        print("You have most likely not input an integer. Please try again.")
+    else:
+        match choice:
+            case 1:
+                name = input("Please input name of customer: ")
+                count = 0
+                for order in orders:
+                    if order.get("Name") == name:
+                        count =+ int(order.get("Price"))
+                print(f"Total price of all orders from {name}: {count}")                
+            case 2:
+                date = input("Please input date (format: DD/MM/YY): ")
+                count = 0
+                for order in orders:
+                    if order.get("Date") == date:
+                        count =+ int(order.get("Price"))
+                print(f"Total price of all orders from {date}: {count}")                
+            case 0:
+                pass
+            case _:
+                print("Incorrect Value.")
+                pass
+    while True:
+        answer = input("Would you like to re-enter the Logistics overview? Y/N: ")
+        answer.strip()
+        if answer == "Y":
+            logistics_overview()
+        elif answer == "N":
+            break
+        else:
+            print("Incorrect choice. Try again.")
+            continue
+    
 def export_file():
     base_filename = input("Enter the filename for the CSV export: ")
     filename = f"{base_filename}.csv"
@@ -115,25 +228,23 @@ def export_file():
 
     try:
         with open(filename, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            
-            # Write the header
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames) #Initializing a writer object so it can export the details to a .csv file
             writer.writeheader()
-
-            # Write the data
-            for order in orders:
+            for order in orders: #Writes data
                 writer.writerow({key: order[key] for key in fieldnames})
 
         print(f"Orders exported to {filename} successfully.")
     except Exception as e:
         print(f"Error exporting file: {e}")
-           
+        
+login()   
 while True: 
-    login()
     answer = input(("Would you like to use the program again? Y/N: "))
+    answer.strip()
     if answer.upper() == "Y":
-       continue
+       login()
     elif answer.upper() == "N":
        break
     else:
-       print("Incorrect Value entered. Will exit program.")
+       print("Incorrect Value entered.")
+       continue
